b9705528413c009d2fc3ff07f14285e84b844cec
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-10-24 11:12:35 +0300

    Update desert.cpp

--- desert.cpp
+++ desert.cpp
@@ -3,19 +3,21 @@
 using namespace std;
 ifstream fin("desert.in");
 ofstream fout("desert.out");
-int N, K, P;
+int N, K;
+float P;
 float desert(int);
 int main()
 {
 	fin >> N >> K >> P;	
 	fin.close();
+	P = P/100;
 	fout << fixed << setprecision(3) << desert(N);
 	fout.close();
 	return 0;
 }
 float desert(int n)
 {
-	if (n <= 2) return (float)n*((100*K)/P);
-	return (float)(100*K)/(P*(2*n-3)) + desert(n-1);
+	if (n <= 2) return n*K/P;
+	return K/(P*(2*n-3)) + desert(n-1);
 }
-// scor 50
+// scor 100
